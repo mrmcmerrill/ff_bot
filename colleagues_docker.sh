79b182f73bcc56f4e58df5599e89863ac9df5da1
Robert McClary <rmcclary.30@gmail.com>
2021-09-21 16:39:46 -0400

    consolidated the envs

--- colleagues_docker.sh
+++
@@ -1,4 +0,0 @@
-source ./colleagues_env
-sudo docker stop colleagues-rankings-bot
-sudo docker rm colleagues-rankings-bot
-sudo docker run --name colleagues-rankings-bot -e "LEAGUE_ID=$LEAGUE_ID" -e "INIT_MSG=$INIT_MSG" -e "LEAGUE_YEAR=$LEAGUE_YEAR" -e "BOT_ID=$BOT_ID" -dit mchome/ff_bot:colleagues
