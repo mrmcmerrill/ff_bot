bdee61b622570274d1ddcddb5282219b8509a8f5
Robert McClary <robert.mcclary@1up.health>
2024-10-01 21:51:28 -0600

    adding space

--- deploy-ff_bot.sh
+++ deploy-ff_bot.sh
@@ -46,10 +46,10 @@ THIS_ESPN_S2="${LEAGUE_NAME}_ESPN_S2"
 DOCKER_CONTAINER=`docker ps -a |grep "${environ}-rankings-bot"`
 TEST_DOCKER_CONTAINER=`docker ps -a |grep "test-rankings-bot"`
 
-if [ "$test" == "true"] && [-z "${$TEST_DOCKER_CONTAINER}"]; then
+if [ "$test" == "true" ] && [ -z "${$TEST_DOCKER_CONTAINER}" ]; then
   sudo docker stop test-rankings-bot
   sudo docker rm test-rankings-bot
-elif [ -z "${DOCKER_CONTAINER}" ] && ["$test" != "true" ]; then
+elif [ -z "${DOCKER_CONTAINER}" ] && [ "$test" != "true" ]; then
   sudo docker stop ${environ}-rankings-bot
   sudo docker rm ${environ}-rankings-bot
 fi
